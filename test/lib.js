88ef031f855d8c282d58071935c931123a3aeb97
Ben Evans <ben@bensbit.co.uk>
2013-12-16 13:12:13 +0000

    Written more tests

--- test/lib.js
+++ test/lib.js
@@ -1,36 +1,104 @@
 'use strict';
 
-var libFapi   = require('../lib/fapi');
-var assert = require('assert');
+var libFapi = require('../lib/fapi');
+var assert  = require('assert');
+var request = require('request');
+var config  = {};
+
+config.port = process.env.PORT || 8000;
 
 describe('Fapi Lib', function() {
   var fapi;
 
   describe('createInstance()', function() {
     it('should return a Fapi instance', function() {
-      fapi = libFapi();
+      fapi = libFapi({
+        publicPath: __dirname + '/public',
+        apiPath: __dirname + '/api',
+        apiRoute: ''
+      });
       assert.ok(fapi instanceof libFapi.Fapi);
+    });
+    it('should provide .server and listen on specified port', function(done) {
+      fapi.server.listen(config.port, done);
     });
   });
 
   describe('Fapi.handleRequest()', function() {
     describe('on a request that should resolve to an api response', function() {
-      it('should return the api file');
-      // request.get(fapi, '/users') === ['bencevans']
-      it('should return an api file when at /');
+      it('should return the api file', function(done) {
+        request({
+          url: 'http://localhost:' + config.port + '/users',
+          json: true
+        }, function(err, res, body) {
+          console.log(body);
+          assert.equal(err, null);
+          assert.deepEqual(body, ['bencevans']);
+          done();
+        });
+      });
+      it('should return an api file when at /', function(done) {
+        request({
+          url: 'http://localhost:' + config.port + '/',
+          json: true
+        }, function(err, res, body) {
+          assert.equal(err, null);
+          assert.deepEqual(body, 'Hello World');
+          done();
+        });
+      });
     });
 
     describe('on a request that should resolve to a static file response', function() {
-      it('should return the static file');
-      it('should allow both .html and without');
-      it('should return an static file when at /');
+      it('should return the static file', function(done) {
+        request({
+          url: 'http://localhost:' + config.port + '/breakfast.html',
+        }, function(err, res, body) {
+          assert.equal(err, null);
+          assert.deepEqual(body, 'Full English');
+          done();
+        });
+      });
+      it('should allow both .html and without', function(done) {
+        request({
+          url: 'http://localhost:' + config.port + '/breakfast',
+        }, function(err, res, body) {
+          assert.equal(err, null);
+          assert.deepEqual(body, 'Full English');
+          done();
+        });
+      });
+      it('should return an static file when at /', function(done) {
+        request({
+          url: 'http://localhost:' + config.port + '/dinner',
+        }, function(err, res, body) {
+          assert.equal(err, null);
+          assert.deepEqual(body, 'Nom');
+          done();
+        });
+      });
     });
 
     describe('on a request that should resolve to a 404 response', function() {
-      it('should return with a statusCode of 404');
-      it('should present a user error');
+      it('should return with a statusCode of 404', function(done) {
+        request({
+          url: 'http://localhost:' + config.port + '/wheres-the-error-yo',
+        }, function(err, res) {
+          assert.equal(err, null);
+          assert.equal(res.statusCode, 404);
+          done();
+        });
+      });
+      it('should present a user error', function(done) {
+        request({
+          url: 'http://localhost:' + config.port + '/wheres-the-error-yo',
+        }, function(err, res, body) {
+          assert.equal(err, null);
+          assert.ok(body.match(/Not Found/));
+          done();
+        });
+      });
     });
-
   });
 
   describe('Fapi.prototype.requestToAPIFilePath', function() {
